9fb23ba6e871083b16d5257045947f5eea9bec0b
kimbirdwon <pswk0907@gmail.com>
2026-03-29 00:28:23 +0900

    azure 스토리지 계정에 공인 IP 추가 및 권한 부여

--- infra/azure/terraform/storage.tf
+++ infra/azure/terraform/storage.tf
@@ -18,6 +18,7 @@ resource "azurerm_storage_account" "raw_storage" {
   network_rules {
     default_action             = "Deny" # 기본적으로 외부 인터넷 접속 모두 차단
     virtual_network_subnet_ids = [azurerm_subnet.consumer_subnet.id] # 오직 컨슈머 서브넷만 허용
+    ip_rules                   = ["218.39.98.40"] # 사용자 공인 IP (Data Lake 업그레이드 후 요구)
   }
 
   tags = {
@@ -30,4 +31,19 @@ resource "azurerm_storage_account" "raw_storage" {
 resource "azurerm_storage_data_lake_gen2_filesystem" "raw_filesystem" {
   name                  = "raw-topic-container"
   storage_account_id    = azurerm_storage_account.raw_storage.id
+ 
+  # 권한이 먼저 부여된 후에 컨테이너를 만들도록 순서 강제
+  depends_on = [
+    azurerm_role_assignment.storage_data_contributor
+  ]
+}
+
+# 현재 실행 중인 내 계정 정보 가져오기
+data "azurerm_client_config" "current" {}
+
+# 내 계정에 '저장소 Blob 데이터 기여가' 권한 자동 부여
+resource "azurerm_role_assignment" "storage_data_contributor" {
+  scope                = azurerm_storage_account.raw_storage.id # 본인의 스토리지 리소스 이름으로 수정
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
